b158c60db8246899daae3cb93da6dfdadb008f6a
BuukunnTT <154568412+WarinthornJ@users.noreply.github.com>
2023-12-22 08:58:27 +0700

    Base Project Sec3

---
+++ Assets/Rocks and Boulders 2/Shaders/RotateGameObject.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+
+public class RotateGameObject : MonoBehaviour {
+	public float rot_speed_x=0;
+	public float rot_speed_y=0;
+	public float rot_speed_z=0;
+	public bool local=false;
+	
+	// Use this for initialization
+	void Start () { 
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+	}
+}
